f56b87c3dac2e5e2f41f24bd6332d201e91ddefd
g-du-1 <42652933+g-du-1@users.noreply.github.com>
2023-04-26 16:48:15 +0100

    Create reddit-ux-fixer.js

---
+++ reddit-ux-fixer.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Reddit UX Fixer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Get rid of annoying Reddit UX.
+// @author       You
+// @match        https://www.reddit.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
