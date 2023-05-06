3564cbb5827a3e50359333ab4b780997c08dcfb7
g-du-1 <dudaszeg1@gmail.com>
2023-05-06 23:16:37 +0100

    Rework, add asserts

--- reddit-ux-fixer.js
+++ reddit-ux-fixer.js
@@ -9,83 +9,179 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+    ("use strict");
+
+    const removeScrollDisabled = () => {
+        document.body.classList.remove("scroll-disabled");
+
+        console.assert(
+            !document.body.classList.contains("scroll-disabled"),
+            "scroll-disabled class is not present on the body"
+        );
+    };
+
+    const clickContinueInBrowser = () => {
+        const continueInBrowserSelector = "button.XPromoPopupRpl__actionButton";
+        const continueBtn = document.querySelector(continueInBrowserSelector);
+
+        if (continueBtn) {
+            continueBtn.click();
+
+            console.assert(
+                !document.querySelector(continueInBrowserSelector),
+                "Continue in browser button is not present"
+            );
+        }
+    };
+
+    const hideBottomBar = () => {
+        const bottomBar = document.querySelector(".XPromoBottomBar");
+
+        if (bottomBar) {
+            bottomBar.style.display = "none";
+
+            console.assert(bottomBar.style.display === "none", "Bottom bar is hidden");
+        }
+    };
+
+    const hideTopAppBtn = () => {
+        const useAppBtn = document.querySelector(".TopNav__useAppButton");
+
+        if (useAppBtn) {
+            useAppBtn.style.display = "none";
+
+            console.assert(useAppBtn.style.display === "none", "Use app button is hidden");
+        }
+    };
+
+    const closeCookieNotice = () => {
+        const cookieNoticeSelector = ".EUCookieNotice__close";
+        const cookieClose = document.querySelector(cookieNoticeSelector);
+
+        if (cookieClose) {
+            cookieClose.click();
+
+            console.assert(!document.querySelector(cookieNoticeSelector), "Close cookie notice button is not present");
+        }
+    };
+
+    const hideSsoIframe = () => {
+        const iframes = document.querySelectorAll("iframe");
+
+        console.assert(iframes.length > 0, "iframes are present");
+
+        Array.from(iframes).forEach((node) => {
+            if (node.src.includes("sso")) {
+                node.style.display = "none";
+
+                console.assert(node.style.display === "none", "SSO iframe is hidden");
+            }
+        });
+    };
+
+    const clickCookieBanner = () => {
+        const cookieBanner = document.querySelector("reddit-cookie-banner");
+
+        if (!cookieBanner) return;
+
+        const shadowRoot = cookieBanner.shadowRoot;
+
+        if (!shadowRoot) return;
+
+        const rejectBtnSelector = "#reject-nonessential-cookies-button > button";
+
+        const rejectBtn = shadowRoot.querySelector(rejectBtnSelector);
+
+        if (!rejectBtn) return;
+
+        rejectBtn.click();
+
+        console.assert(!shadowRoot.querySelector(rejectBtnSelector), "Cookie banner is not present");
+    };
+
+    const hideOauthModal = () => {
+        const oauthModal = document.querySelector("#credential_picker_iframe");
+
+        if (!oauthModal) return;
+
+        oauthModal.style.display = "none";
+
+        console.assert(oauthModal.style.display === "none", "Oauth modal is hidden");
+    };
+
+    const hidePostAppBtn = (allAsyncLoaders) => {
+        Array.from(allAsyncLoaders).forEach((node) => {
+            if (node.paintGroup === "xpromo" && node.bundleName === "top_button") {
+                node.style.display = "none";
+
+                console.assert(node.style.display === "none", "Post app button is hidden");
+            }
+        });
+    };
+
+    const hideBottomPromoBar = (allAsyncLoaders) => {
+        const botPromoBar = Array.from(allAsyncLoaders).find((node) => node.bundleName === "bottom_bar_xpromo");
+
+        if (botPromoBar) {
+            botPromoBar.style.display = "none";
+
+            console.assert(botPromoBar.style.display === "none", "Bottom promo bar is hidden");
+        }
+    };
+
+    const clickMoreComments = (allFacePlatePartials) => {
+        Array.from(allFacePlatePartials).forEach((node) => {
+            if (node.method === "post" && node.src.includes("top-level=1") && checkVisible(node)) {
+                node.click();
+
+                // TODO: Assert
+            }
+        });
+    };
+
+    const getAllAsyncLoaders = () => {
+        return document.querySelectorAll("shreddit-async-loader");
+    };
+
+    const getAllFacePlatePartials = () => {
+        return document.querySelectorAll("faceplate-partial");
+    };
 
     const checkVisible = (elm) => {
         var rect = elm.getBoundingClientRect();
         var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
         return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
-    }
+    };
 
     const unfuckUI = () => {
-        console.log('Unfucking UI...')
+        console.log("Unfucking UI...");
 
-        // LISTING
+        // Listing
 
-        const html = document.querySelector('html');
-        html.classList.remove('scroll-disabled');
-        const body = document.querySelector('body');
-        body.classList.remove('scroll-disabled');
+        removeScrollDisabled();
+        clickContinueInBrowser();
+        hideBottomBar();
+        hideTopAppBtn();
+        closeCookieNotice();
+        hideSsoIframe();
 
-        const iframes = document.querySelectorAll('iframe')
+        // Post Page
 
-        Array.from(iframes).forEach(node => {
-           if (node.src.includes('sso')) {
-               node.style.display = 'none';
-           }
-        });
+        clickCookieBanner();
+        hideOauthModal();
 
-        const continueBtn = document.querySelector('button.XPromoPopupRpl__actionButton');
-        if (continueBtn) continueBtn.click();
+        const allAsyncLoaders = getAllAsyncLoaders();
 
-        const bottomBar = document.querySelector('.XPromoBottomBar');
-        if (bottomBar) bottomBar.style.display = 'none';
+        hidePostAppBtn(allAsyncLoaders);
+        hideBottomPromoBar(allAsyncLoaders);
 
-        const useAppBtn = document.querySelector('.TopNav__useAppButton');
-        if (useAppBtn) useAppBtn.style.display = 'none';
+        const allFacePlatePartials = getAllFacePlatePartials();
 
-        const cookieClose = document.querySelector('.EUCookieNotice__close');
-        if (cookieClose) cookieClose.click();
+        clickMoreComments(allFacePlatePartials);
+    };
 
-        // POST PAGE
-
-        // Top Cookies
-        const topCookies = document.querySelector("reddit-cookie-banner")?.shadowRoot.querySelector("#reject-nonessential-cookies-button > button")
-        if (topCookies) topCookies.click()
-
-        // Oauth Modal
-        const oauthModal = document.querySelector('#credential_picker_iframe');
-        if (oauthModal) oauthModal.style.display = 'none';
-
-        const allAsyncLoaders = document.querySelectorAll('shreddit-async-loader');
-
-        // Top Use App Btn
-        Array.from(allAsyncLoaders).forEach(node => {
-           if (node.paintGroup === 'xpromo' && node.bundleName === 'top_button') {
-               node.style.display = 'none';
-           }
-        });
-
-        // Bottom Promo Bar
-        const botPromoBar = Array.from(allAsyncLoaders).find(node => node.bundleName === 'bottom_bar_xpromo');
-        if (botPromoBar) botPromoBar.style.display = 'none';
-
-        // Bottom Continue
-        // const botContinue = document.querySelector("body > shreddit-app > shreddit-experience-tree").shadowRoot.querySelector("div > shreddit-async-loader").shadowRoot.querySelector("xpromo-app-selector")?.shadowRoot.querySelector("#secondary-button")
-        // if (botContinue) botContinue.click()
-
-        // Load More Comments
-        const allFacePlatePartials = document.querySelectorAll('faceplate-partial');
-
-        Array.from(allFacePlatePartials).forEach(node => {
-           if (node.method === 'post' && node.src.includes('top-level=1') && checkVisible(node)) {
-               node.click()
-           }
-        });
-    }
-
-    document.addEventListener('touchstart', unfuckUI)
-    document.addEventListener('keydown', unfuckUI)
-    document.addEventListener('input', unfuckUI)
+    document.addEventListener("touchstart", unfuckUI);
+    document.addEventListener("keydown", unfuckUI);
+    document.addEventListener("input", unfuckUI);
 })();
